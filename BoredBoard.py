9f66fb11defe6740c93e7fab1e5537d43d7b798a
Froom2 <nicole@avison.co.uk>
2015-08-18 20:51:48 +0100

    Added while loop and finished user input.
    Also convert response to all lowercase.

--- BoredBoard.py
+++ BoredBoard.py
@@ -14,7 +14,17 @@ tasks = ["study Java.",
          "clean the kitchen surfaces.",
          "mop a floor.",
          "do some decorating.",
-         "play guitar."]
-print("You should", random.choice(tasks))
+         "play guitar.",
+         "Learn Python!"]
 
-input("Press enter to exit")
+print("You should", random.choice(tasks))
+answer = str.lower(input("Do you want to do that?"))
+
+while answer != "yes":
+    print("You should", random.choice(tasks))
+    answer = str.lower(input("Do you want to do that?"))
+    if answer == "yes":
+        break
+
+print("So now go do that!")
+
