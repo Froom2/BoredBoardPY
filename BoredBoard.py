03c1dcaa265efd7b0ee32cce4dad4d0b5ce8eac8
Froom2 <nicole@avison.co.uk>
2015-08-18 20:51:48 +0100

    added while loop and finished user input

--- BoredBoard.py
+++ BoredBoard.py
@@ -14,7 +14,16 @@ tasks = ["study Java.",
          "clean the kitchen surfaces.",
          "mop a floor.",
          "do some decorating.",
-         "play guitar."]
-print("You should", random.choice(tasks))
+         "play guitar.",
+         "Learn Python!"]
 
-input("Press enter to exit")
+print("You should", random.choice(tasks))
+answer = input("Do you want to do that?")
+
+while answer != "yes":
+    print("You should", random.choice(tasks))
+    answer = input("Do you want to do that?")
+    if answer == "yes":
+        break
+
+print("So now go do that!")
