26e80c504981dc7c6575dea0a6c62c8c5b956bbf
Froom2 <nicole@avison.co.uk>
2015-08-18 20:41:35 +0100

    Added basic tasks and user input.

--- BoredBoard.py
+++ BoredBoard.py
@@ -1,1 +1,20 @@
+import random
+
 print('Welcome to the Bored Board!')
+
+tasks = ["study Java.",
+         "wash up.",
+         "listen to a podcast.",
+         "empty a box!",
+         "read a book.",
+         "do your dailies on GuildWars.",
+         "hoover.",
+         "clean the bathtub and sink.",
+         "clean the toilet.",
+         "clean the kitchen surfaces.",
+         "mop a floor.",
+         "do some decorating.",
+         "play guitar."]
+print("You should", random.choice(tasks))
+
+input("Press enter to exit")
