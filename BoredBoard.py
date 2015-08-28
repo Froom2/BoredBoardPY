456cbac85f4282503c8679377b917a8a53063d1d
Froom2 <nicole@avison.co.uk>
2015-08-28 20:01:21 +0100

    Updated yes/no choice in suggestion function. Reformatted and commented lines.

--- BoredBoard.py
+++ BoredBoard.py
@@ -8,7 +8,7 @@ tasks = ["study Java.",
          "listen to a podcast.",
          "empty a box!",
          "read a book.",
-         "do your dailies on GuildWars.",
+         "learn some more Python.",
          "hoover.",
          "clean the bathtub and sink.",
          "clean the toilet.",
@@ -18,13 +18,14 @@ tasks = ["study Java.",
          "play guitar.",
          "Learn Python!"]
 
+
 def introduction():
     # Introduction to the program
     print('Welcome to the Bored Board!')
     print("""
     1 - View activities
     2 - Add an activity
-    3 - Remove and Activity
+    3 - Remove an Activity
     4 - Change an activity
     5 - Get suggestions
     6 - Exit
@@ -32,48 +33,58 @@ def introduction():
     option = int(input("What would you like to do? "))
     return option
 
+
 def printtasks():
+    # Print tasks
     for i in tasks:
         print(i)
 
-def suggest():
-    print("You should", random.choice(tasks))
-    answer = str.lower(input("Do you want to do that?"))
 
-    # TODO Insert yes/ no specifier and error check to catch.
+def suggest():
+    # Runs suggestion loop
+    print("You should", random.choice(tasks))
+    answer = str.lower(input("Do you want to do that? (yes/no) "))
 
     while answer != "yes":
         print("You should", random.choice(tasks))
-        answer = str.lower(input("Do you want to do that?"))
+        answer = str.lower(input("Do you want to do that? (yes/no) "))
+        if answer != "yes":
+            print("you said ", answer, "...")
         if answer == "yes":
             break
 
     print("So now go do that!")
 
+# Introduction
 optionchoice = introduction()
 
+# Option 1 - Print tasks, then return to introduction
 if optionchoice == 1:
     printtasks()
 
+# Option 2 - Add an activity, then return to introduction
 elif optionchoice == 2:
     print("Function not added yet")
 
+# Option 3 - Remove activity, then return to introduction
 elif optionchoice == 3:
     print("Function not added yet")
 
+# Option 4 - Change an activity, then return to introduction
 elif optionchoice == 4:
     print("Function not added yet")
 
+# Option 5 - Run suggestions, then exit
 elif optionchoice == 5:
     suggest()
 
+# Option 6 - Exit
 elif optionchoice == 6:
+    # TODO functionalise this then put at the end of the other things it needs to be in
     input("Press any key to confirm exit")
 
+# Other - Print error then return to introduction
 else:
     print("Sorry that isn't a valid choice.")
 
 # TODO Enable changing/adding/removing things
-
-
-
